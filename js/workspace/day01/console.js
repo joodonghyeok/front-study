36026a01889818d1af0684fc1dbc0858e4609052
joodonghyeok <cmh506@naver.com>
2025-03-14 01:41:39 +0900

    js 01 마무리

--- js/workspace/day01/console.js
+++ js/workspace/day01/console.js
@@ -1,1 +1,4 @@
-console.log("hello, world! 😎")
+console.log("hello, world!😎")
+console.log("주동혁! \n화이팅!")
+console.log("스테이크\t굽자!")
+// 탭키 라고 생각하면 된다
---
+++ js/workspace/day01/variable.js
@@ -0,0 +1,3 @@
+// 변수(변하는 수)
+let name = "홍길동"
+console.log(name)
