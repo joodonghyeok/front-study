77379b51b4122ca03a35fd8f071f4b45b89d26e4
joodonghyeok <cmh506@naver.com>
2025-03-16 07:50:01 +0900

    js day02

--- js/workspace/day01/variable.js
+++ js/workspace/day01/variable.js
@@ -1,3 +1,45 @@
 // 변수(변하는 수)
-let name = "홍길동"
-console.log(name)
+// let name = "홍길동"
+// let firstName = "홍"
+// console.log(name)
+
+// let age = 20;
+// age = 10;
+
+// console.log(age)
+
+// let score = 20;
+// let average = 20.5;
+// let name2 = "홍길동"
+
+// var a;
+// // var a;
+// // var a;
+// // var a;
+// // var a;
+
+// let a;
+// // let a;
+// // let a;
+// // let a;
+// // let a;
+
+// 호이스팅 순서 (단골 면접 질문이다.)
+// console.log(name)
+// var name = "홍길동"
+
+
+// console.log(name)
+// let name = "홍길동"
+
+console.log(typeof(42))
+console.log(typeof(42.000))
+console.log(typeof(true))
+console.log(typeof("홍길동"))
+console.log(typeof("홍"))
+
+console.log("20" + 20)
+console.log("20" - 20)
+console.log("ㄱ" - 20)
+
+
---
+++ js/workspace/day02/oper/operator.js
@@ -0,0 +1,65 @@
+let num1 = 20
+let num2 = 30
+let num3 = 5
+
+// 최우선 연산자
+// console.log(num1 * (num2 + num3))
+
+//증감 연산자
+//전위 ,후위 연산자
+
+//후위식
+// let data = 10
+// console.log(data++)
+// console.log(data)
+
+//전위식
+// let data = 10
+// console.log(++data)
+// console.log(data)
+
+// //not 연산자
+// console.log(!true)
+
+
+
+// console.log(10+50) // 연산
+// console.log(2+월) // 연결
+
+// console.log(2*월) // NaN
+// console.log(2*"20") // 40
+
+// // 몫
+// console.log(100/15) 
+// // 나머지
+// console.log(100%15) 
+
+//누적복합연산자
+// let data5 = 1000
+// data5 +=1000
+// console.log(data5)
+
+// 관계연산자
+// 결과값이 boolean
+// 동등비교 : 값
+console.log("10"==10)
+// 일치비교 : 타입
+console.log("10"===10)
+
+
+console.log("10"!==10)
+
+let name = false ||  "홍길동 "// 초기값 설정할 떄 사용한다 값을 받아올 때 사용하면 좋을 거 같다....!
+let name2 = "주동혁" || "홍길동"
+console.log(name)
+console.log(name2)
+
+
+
+
+
+
+
+
+
+
